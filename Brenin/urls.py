c6007ed8bdfa986d4a92123d4b669865818a14fa
akashtilak0209 <107496023+akashtilak0209@users.noreply.github.com>
2022-06-17 10:43:18 +0530

    Fixed bug in urls.py
    Speech Recognition features needs to be worked upon. Commenting it for now.

--- Brenin/urls.py
+++ Brenin/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
     path('submit/', views.submitquery, name='submit'),
-    path('speechrecog', views.speechrecog, name="speech")
+#     path('speechrecog', views.speechrecog, name="speech")
 ]
